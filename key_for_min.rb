b43f879b4073572783b4cbf979a7b838b6391697
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-11 22:55:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,12 @@
 def key_for_min_value(name_hash)
   if name_hash == {}
          return nil
-      else
-         name_hash= name_hash.map {|k , v| v}
-         name_hash[0][0]
+        min_key,min_value = name_hash.first
+        name_hash.each do |k, v|
+      if value < min_value
+        min_key = key 
        end
      end 
+     min_key
+   end 
+  
