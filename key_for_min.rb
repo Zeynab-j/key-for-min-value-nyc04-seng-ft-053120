bfed9a520abbb56230464ccab0b3443580b400aa
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-11 22:10:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
          return nil
     else
-     name_hash= name_hash.sort_by {|k , v| v}
+     name_hash= name_hash.select {|k , v| v}
      name_hash[0][0]
      end
   end
