acb17cc9f49fa00eee498bf49c89d9e0f2d60e1e
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-11 22:59:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash.size == 0 
+  return nil if name_hash.size == 0
         min_key, min_value = name_hash.first
         name_hash.each do |k, v|
       if value < min_value
@@ -11,4 +11,3 @@ def key_for_min_value(name_hash)
      end
      min_key
    end
- 
