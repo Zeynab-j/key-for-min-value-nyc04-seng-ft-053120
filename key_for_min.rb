732e2b428ace56c3d27ce7b32a76996a9e8879fe
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-11 22:55:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,9 @@ def key_for_min_value(name_hash)
         min_key,min_value = name_hash.first
         name_hash.each do |k, v|
       if value < min_value
-        min_key = key 
+        min_key = key
        end
-     end 
+     end
      min_key
-   end 
+   end
   
